ab15922c4a170ba70496f64d91a7a9c8e3de2129
John Miclat <73535495+midehl@users.noreply.github.com>
2024-02-13 01:26:22 -0500

    fix copilote

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,18 @@
 return {
-  {
-    'zbirenbaum/copilot-cmp',
-    event = { 'BufEnter' },
-    dependencies = { 'zbirenbaum/copilot.lua' },
-    config = function()
-      require('copilot_cmp').setup()
-    end
-  }
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		event = { "BufEnter" },
+		dependencies = { "zbirenbaum/copilot.lua" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
