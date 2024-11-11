d280c7e356aac04be2633176314184b1907dab22
John Miclat <73535495+midehl@users.noreply.github.com>
2024-11-11 00:38:13 -0500

    update aliases

--- .config/zsh/zsh_aliases.zsh
+++ .config/zsh/zsh_aliases.zsh
@@ -1,29 +1,41 @@
-
-# -- [[ ALIASES ]] --
-
+# MISC ------------------------------
 alias notes="cd $HOME/Notes && vim"
 alias notescd="cd $HOME/Notes"
 alias c=clear
 alias lzd='lazydocker'
+alias t=tmux
+alias ta='tmux attach -t'
+alias d=docker
+alias rm=trash-put
 
-
-# lol lazy asl
+# GIT ------------------------------
+# Status
+alias ff='gpr && git pull --ff-only'
 alias gs='git status'
+alias gl='git log --all --graph --format=oneline'
 alias gd='git diff'
+alias gds='git diff --staged'
+alias grs='git restore --staged'
+# Committing
 alias gad='git add '
 alias gal='git add .'
 alias gcm='git commit -m'
+# Push/Pull
 alias gp='git pull'
+alias gP='git push' # capitalize push for safety
+alias gpf='git push --force-with-lease'
 alias gpr='git pull --rebase' # keep cleaner commit history
+# GIT Branch
 alias gb='git branch'
 alias gba='git branch -a'
 alias gco='git checkout'
 alias gcb='git checkout -b'
+alias gpr='git remote prune origin'
 alias gm='git merge'
+# Other
 alias gstash='git stash'
-alias gl='git log'
-alias gP='git push' # capitalize push for safety
 
+# Go places lol------------------------------
 alias dotfiles="cd $HOME/.dotfiles/"
 alias zshconfig="nvim $HOME/.config/zsh/.zshrc"
 alias zshup="source $HOME/.config/zsh/.zshrc"
@@ -34,7 +46,7 @@ alias sd="cd ~ && cd \$(find * -type d | fzf)"
 alias vim=nvim
 
 
-# Exa/LS aliases
+# Exa/LS------------------------------
 alias ls='exa'                                                          # ls
 alias l='exa -lbF --git'                                                # list, size, type, git
 alias ll='exa -lbGF --git'                                              # long list
