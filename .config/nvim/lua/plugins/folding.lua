59ae3c11b797f5c9d9829833da550419ff8f882a
John Miclat <73535495+midehl@users.noreply.github.com>
2024-11-14 00:32:10 -0500

    add file to ignore

--- .config/nvim/lua/plugins/folding.lua
+++ .config/nvim/lua/plugins/folding.lua
@@ -69,6 +69,9 @@ return {
         if ft == "dashboard" then
           return ""
         end
+        if ft == "conf" then
+          return ""
+        end
         if ft == "log" then
           return ""
         end
