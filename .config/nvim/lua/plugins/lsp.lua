6eed9ff034ae56bcb965fd3ff9a2f36ff36026ba
John Miclat <73535495+midehl@users.noreply.github.com>
2024-11-18 12:46:56 -0500

    add error diagnostic keymap

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<leader>E", "<cmd>lua vim.diagnostic.open_float()<CR>", desc = "Open Diagnostic Float" }
+    end,
+  },
+}
