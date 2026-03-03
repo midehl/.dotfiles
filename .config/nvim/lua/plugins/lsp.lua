4a5c935d000084554c6bfb65930ac7c368da59b6
John Miclat - j0m1tzn <73535495+midehl@users.noreply.github.com>
2026-03-02 16:25:11 -0800

    Update lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,9 +2,14 @@ return {
   -- LSP keymaps
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", desc = "Open Diagnostic Float" }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", has = "definition" },
+          },
+        },
+      },
+    },
   },
 }
