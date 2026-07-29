ba8545bf27076f256b230b53c35d66566f313950
John Miclat <73535495+midehl@users.noreply.github.com>
2026-07-29 03:08:17 -0700

    nvim lazygit sizing

--- .config/nvim/lua/plugins/git_stuff.lua
+++ .config/nvim/lua/plugins/git_stuff.lua
@@ -2,4 +2,13 @@ return {
   {
     "tpope/vim-fugitive",
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      -- full-screen lazygit (<leader>gg), matching herdr/tmux behavior
+      lazygit = {
+        win = { width = 0, height = 0 },
+      },
+    },
+  },
 }
